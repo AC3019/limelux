67a49915c97e5f4c6c5240393166314d929d3aee
hanyue1014 <wongxuanbin@gmail.com>
2021-10-11 22:38:29 +0800

    Added more animations to the header (shrink and float on scroll)

--- src/js/global.js
+++ src/js/global.js
@@ -1,4 +1,20 @@
 /*
+  Apply the "shrink and float header bar"
+    - make the window listen for onscroll function
+    - check the scroll on body and the documentElement AKA html itself
+    - if the scrollTop is big, apply the header-scrolled class
+*/
+
+window.onscroll = () => {
+  if (document.body.scrollTop >= 60 || document.documentElement.scrollTop >= 60) {
+    document.getElementsByTagName('header')[0].classList.add('header-scrolled')
+  } else {
+    // if the scroll is not that big then the class shall be removed
+    document.getElementsByTagName('header')[0].classList.remove('header-scrolled')
+  }
+}
+
+/*
   Nav styles
     - add event listener to every nav links
     - it will toggle the active class on them when they are hovered
